d607ce009f4c2fa67bbbf9589293ce255e821028
emlylam <emly@connect.hku.hk>
2018-05-31 11:38:12 +0800

    Header menu added to layouts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,44 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import Link from 'gatsby-link'
 
-import Header from '../components/header'
+const ListLink = props =>
+  <li style={{ display: 'inline-block', marginRight: '1rem' }}>
+    <Link to={props.to}>
+      {props.children}
+    </Link>
+  </li>
 
-const Layout = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+export default ({ children }) => (
+  <div style={{margin: '0 auto', maxWidth: 650, padding: '1.25rem 1rem'}}>
+    <header style={{ marginBottom: '1.5rem' }}>
+      <Link to="/" style={{ textShadow: 'none', backgroundImage: 'none' }}>
+        <h3 style={{ display: 'inline' }}>Emily Lam</h3>
+      </Link>
+      <ul style={{ listStyle: 'none', float: 'right' }}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/projects/">Projects</ListLink>
+      </ul>
+    </header>
+    {children()}
   </div>
-)
 
-Layout.propTypes = {
-  children: PropTypes.func,
-}
-
-export default Layout
-
-export const query = graphql`
-  query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+);
