1575cb62ff86f5aaa3f4560979e0d5956330062e
emlylam <emly@connect.hku.hk>
2018-06-05 11:49:19 +0800

    Added favicon plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,5 +19,23 @@ module.exports = {
         maxWidth: 1080,
       },
     },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        },
+      },
+    },
   ],
 }
