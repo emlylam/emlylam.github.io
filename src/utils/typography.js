b5d59da24c2d5e2624f4db9c77f6daaea978c13c
emlylam <emly@connect.hku.hk>
2018-06-13 14:23:58 +0800

    Header remove bottom margin and bottom padding

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -4,8 +4,6 @@ const typography = new Typography({
   baseFontSize: '16px',
   headerFontFamily: ['Helvetica Neue'],
   bodyFontFamily: ['Helvetica Neue'],
-  bodyHeight: '100%',
-
 });
 
 export default typography;
