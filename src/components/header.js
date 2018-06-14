e2f662dc385efa48901ea75f390704dbfa434b27
emlylam <emly@connect.hku.hk>
2018-06-14 17:23:28 +0800

    Responsive header + other responsiveness

--- src/components/header.js
+++ src/components/header.js
@@ -3,29 +3,60 @@ import Link from 'gatsby-link'
 import styles from './header.module.css'
 import { NavLink } from 'react-router-dom'
 
-const ListLink = props =>
-  <li className={styles.liStyle}>
-    <NavLink
-      exact to={props.to}
-      className={styles.navLinkStyle}
-      activeStyle={{borderBottomWidth: '2.5px', borderBottomStyle: 'solid'}}
-    >
-      {props.children}
-    </NavLink>
-  </li>
+// const ListLink = props =>
+//   <li className={styles.liStyle}>
+//     <NavLink
+//       exact to={props.to}
+//       className={styles.navLinkStyle}
+//       activeStyle={{borderBottomWidth: '2.5px', borderBottomStyle: 'solid'}}
+//     >
+//       {props.children}
+//     </NavLink>
+//   </li>
+//
+// export default ({children}) => (
+//   <header className={styles.headerLeft}>
+//     <Link to="/" style={{ textDecoration: 'none', textShadow: 'none', backgroundImage: 'none' }}>
+//       <h3 className={styles.logoStyle}>emly</h3>
+//     </Link>
+//     <div className={styles.headerRight}>
+//       <ul className={styles.headerRightUL}>
+//         <ListLink exact to="/about/">About</ListLink>
+//         <ListLink exact to="/projects/">Projects</ListLink>
+//         <ListLink exact to="/contact/">Contact</ListLink>
+//       </ul>
+//     </div>
+//   </header>
+// )
+
+
+const MenuLink = props =>
+  <NavLink
+    exact to={props.to}
+    className={styles.menuLink}
+    activeStyle={{borderBottomWidth: '2.5px', borderBottomStyle: 'solid'}}
+    // activeStyle={{backgroundColor: 'yellow'}}
+  >
+    {props.children}
+  </NavLink>
 
 export default ({children}) => (
-  <header className={styles.headerStyle}>
-    <Link to="/" style={{ textDecoration: 'none', textShadow: 'none', backgroundImage: 'none' }}>
-      <h3 className={styles.logoStyle}>emly</h3>
+  <header className={styles.header}>
+    <Link to="/"
+          style={{ textDecoration: 'none', textShadow: 'none', backgroundImage: 'none' }}
+          className={styles.logo}
+    >
+      <h3 className={styles.logoText}>emly</h3>
     </Link>
-    <ul className={styles.ulStyle}>
-      <ListLink exact to="/about/">About</ListLink>
-      <ListLink exact to="/projects/">Projects</ListLink>
-      <ListLink exact to="/contact/">Contact</ListLink>
-    </ul>
+    <div className={styles.headerRight}>
+        <MenuLink exact to="/about/" className={styles.about}>About</MenuLink>
+        <MenuLink exact to="/projects/" className={styles.projects}>Projects</MenuLink>
+        <MenuLink exact to="/contact/" className={styles.contact}>Contact</MenuLink>
+    </div>
   </header>
 )
+
+
 
 
 
